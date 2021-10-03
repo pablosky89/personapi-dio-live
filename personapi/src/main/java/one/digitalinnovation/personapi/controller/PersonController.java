4937a326d1710a644a9358dc0c78b2188f9cc080
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-10-03 17:11:42 -0300

    developed person create operation

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.entity.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 
     @GetMapping
-    public String getBook(){
+    public String getBook() {
         return "API test";
     }
 
