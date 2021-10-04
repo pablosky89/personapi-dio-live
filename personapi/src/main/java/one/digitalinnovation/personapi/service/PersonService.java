88089fed9cfea4e62e6e788a120926e9afbdd841
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-10-04 16:15:54 -0300

    DTO Class created

--- personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,14 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson( Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = Person.builder()
+                .firstName(personDTO.getFirstName())
+                .lastName(personDTO.getLastName())
+                .birthDate(personDTO.getBirthDate())
+                .phones(personDTO.getPhones())
+                .build();
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Create person with ID "+ savedPerson.getId())
