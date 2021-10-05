aa6f9c316b3e582249fab70be2b6e0d2fbd4cda1
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-10-05 16:12:33 -0300

    Developed get person by ID

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.personapi.controller;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,4 +33,8 @@ public class PersonController {
         return personService.listAll();
     }
 
+    @GetMapping(value = "/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
 }
